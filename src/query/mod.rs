6694d32d1839e26a5fb16447740e100b73c96a5e
Traines <git@traines.eu>
2023-12-03 16:27:20 +0000

    refactor for multiple query algos

---
+++ src/query/mod.rs
@@ -0,0 +1,10 @@
+pub mod recursive;
+pub mod csa;
+
+use crate::distribution_store;
+use crate::connection;
+use crate::types;
+
+pub fn query<'a>(store: &'a mut distribution_store::Store, origin: &'a connection::Station<'a>, destination: &'a connection::Station<'a>, start_time: types::Mtime, max_time: types::Mtime, now: types::Mtime) {
+    recursive::query(store, origin, destination, start_time, max_time, now);
+}
